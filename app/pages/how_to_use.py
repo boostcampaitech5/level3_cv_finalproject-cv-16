8b6a563910f340aa16e0ef56ada5fa6c82662665
97DongHyeokOH <donggar97@naver.com>
2023-07-24 07:40:27 +0000

    hot_fix

--- app/pages/how_to_use.py
+++ app/pages/how_to_use.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # 로컬 동영상 파일 경로
-video_path = "/home/donggar97/level3_cv_finalproject-cv-16/app/how_to_use.mp4"
+video_path = "../image_video/how_to_use.mp4"
 
 # 동영상 표시
 st.write('## 사용 방법')
